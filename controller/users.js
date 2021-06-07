62b306a676e6ecc204eb4ba690625641e5460803
Eduardo Molina <edrdmolina@icloud.com>
2021-06-07 15:53:16 -0400

    Add routes for Users

--- controller/users.js
+++ controller/users.js
@@ -1,1 +1,24 @@
-const User = require()
+const User = require('../models/user');
+
+
+module.exports = {
+    getLogin: (req, res, next) => {
+        res.render('users/login');
+    },
+    postLogin: (req, res, next) => {
+        res.send('post login form')
+    },
+    getRegister: (req, res, next) => {
+        res.render('users/register');
+    },
+    postRegister: (req, res, next) => {
+        res.send('post register form');
+    },
+    getProfile: (req, res, next) => {
+        res.render('users/profile');
+    },
+    putProfile: (req, res, next) => {
+        res.send('put profile form')
+    }
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const { 
+    getLogin, postLogin,
+    getRegister, postRegister,
+    getProfile, putProfile,
+} = require('../controller/users');
+
+
+// GET login /users/login
+router.get('/login', getLogin);
+
+// POST login /users/login
+router.post('/login', postLogin); 
+
+// GET register /users/register
+router.get('/register', getRegister);
+
+// POST register /users/register
+router.post('/register', postRegister);
+
+// GET profile /users/profile
+router.get('/profile', getProfile);
+
+// PUT profile /users/profile
+router.put('/profile', putProfile);
 
 module.exports = router;
